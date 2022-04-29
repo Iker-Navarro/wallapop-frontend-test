694ea9f2200b8174eb5fa9860fe9a6946072a3be
iker-navarro <iknamo2510@gmail.com>
2022-04-29 18:55:04 +0200

    feat: change header styles and content

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -5,25 +5,23 @@ import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { NgModule } from '@angular/core';
 
+const materialModules = [
+  MatSnackBarModule,
+  MatTableModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatDialogModule
+]
+
 @NgModule({
-  imports: [
-    MatSnackBarModule,
-    MatTableModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatPaginatorModule
-  ],
-  exports: [
-    MatSnackBarModule,
-    MatTableModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatPaginatorModule
-  ],
+  imports: materialModules,
+  exports: materialModules
 })
 export class MaterialModule {}
